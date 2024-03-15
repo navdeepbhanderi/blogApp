852970b6ef591e58ffc6a1691b21e369cd2b715e
navdeep bhanderi <navdeepbhanderi1@gmail.com>
2024-03-15 19:41:45 +0530

    changesForDeployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { signupComponent } from './signup/signup.component';
 import { blogComponent } from './blog/blog.component';
 import { blogDetailsComponent } from './blogDetails/blogDetails.component';
-import { blogHomeComponent } from './blogHome/bloghome.component';
 import { signinComponent } from './signin/signin.component';
 import { blogDataService } from './shared/blogData.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -16,6 +15,7 @@ import { blogTemplateComponent } from './blogTemplate/blogTemplate.component';
 import { errorComponent } from './errorComponent/errorComponent.component';
 import { authService } from './shared/authService.service';
 import { AddClassesDirective } from './blogDetails/blog.directive';
+import { blogHomeComponent } from './blogHomeComponent/blogHomeComponent.component';
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/blogHomeComponent/blogHomeComponent.component.ts
+++ src/app/blogHomeComponent/blogHomeComponent.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-blog-home',
-  templateUrl: './blogHome.component.html',
-  styleUrls: ['./blogHome.component.scss'],
+  templateUrl: './blogHomeComponent.component.html',
+  styleUrls: ['./blogHomeComponent.component.scss'],
 })
 export class blogHomeComponent {}
